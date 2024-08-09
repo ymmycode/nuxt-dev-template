3b7cdb4297e43e76d3c144c80ae2b5be6cafbe40
Achmat Fauzi <achmatfauzi86@gmail.com>
2024-08-09 15:52:43 +0700

    update index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
+  <div class="title">
     Hello
   </div>
 </template>
 
 <script setup>
+const { $gsap } = useNuxtApp()
 
+onMounted(() => {
+  $gsap.from('.title', { x: -100, duration: 1 })
+})
 </script>
 
 <style lang="scss" scoped>
